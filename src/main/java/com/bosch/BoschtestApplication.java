af7ae6e78e0a1578903390e73f023b66f7b6e470
Patrick Grigat <patrick.grigat@emc.com>
2016-04-22 14:28:45 +0200

    Test

---
+++ src/main/java/com/bosch/BoschtestApplication.java
@@ -0,0 +1,12 @@
+package com.bosch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoschtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoschtestApplication.class, args);
+	}
+}
